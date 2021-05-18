fabd675fe95f7e63fb29ad516a42be7ead7e491f
Jose Miguel Armijo <jm.armijo.f@gmail.com>
2021-05-18 22:55:49 +1000

    Update README file with manual steps to populate database Parse dates to the right format

--- README.md
+++ README.md
@@ -14,6 +14,33 @@ cd csv_parser
 python parser.py 'jobs.csv'
 ```
 
+### Load data
+
+These are manual steps for now:
+
+```
+docker cp t_jobs.csv coding-challenge-db:/tmp
+docker cp t_files.csv coding-challenge-db:/tmp
+```
+
+```
+mysql --host=localhost --user=root --password=your_mysql_root_password
+CREATE DATABASE jobs_board;
+
+create table jobs (title VARCHAR(100), description VARCHAR(200), location VARCHAR(100), date DATE);
+create table applicants (id INTEGER, applicant VARCHAR(100));
+
+LOAD DATA LOCAL INFILE '/tmp/t_jobs.csv' into table jobs FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+LOAD DATA LOCAL INFILE '/tmp/t_applicants.csv' into table applicants FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+```
+
+## Future work:
+- Have a user with less privileges (instead of using root)
+- Remove passwords from the repository
+- Create primary/foreign keys and indexes in tables
+- When parsing, beware of commas withing the values (Ex: location: "Docklands, Melbourne")
+- Tune the size of each column to avoid wasting space: consider using other data types perhaps.
+
 ## Development
 * React frontend: http://localhost
 * Lumen backend: http://localhost:8000
--- csv_parser/parser.py
+++ csv_parser/parser.py
@@ -1,10 +1,11 @@
 import sys
 import csv
+from datetime import datetime
 
 class CSVParser:
     def __init__(self):
         self.jobs_file = "t_jobs.csv"
-        self.applicants_file = "t_files.csv"
+        self.applicants_file = "t_applicants.csv"
 
         self.jobs_file_handler = None
         self.applicants_file_handler = None
@@ -44,19 +45,24 @@ class CSVParser:
 
     def write_job(self, idx, line, map_headers):
         self.jobs_file_handler.write("{},{},{},{},{}\n".format(
-            idx,
+            idx+1,
             line[map_headers['job title']],
             line[map_headers['job description']],
             line[map_headers['location']],
-            line[map_headers['date']]
+            self.convert_date(line[map_headers['date']])
         ))
+
+    def convert_date(self, date):
+        parsed_date = datetime.strptime(date, '%d/%m/%Y')
+        return parsed_date.strftime('%Y-%m-%d')
 
     def write_aplicants(self, idx, line, map_headers):
         applicants = [x.strip() for x in line[map_headers['applicants']].split(',')]
 
         for applicant in applicants:
             self.applicants_file_handler.write("{},{}\n".format(
-                idx, applicant
+                idx+1,
+                applicant
             ))
 
 # TODO : Do argument validation and add a help menu
